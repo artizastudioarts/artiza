73d59b0f62f33c75c447c39251a504c7b311973d
artizastudioarts <artiza.studioarts@gmail.com>
2026-07-30 03:46:58 +0200

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import Link from "next/link";
-import Image from "next/image";
 import { useCart } from "@/context/CartContext";
 
 export default function Header() {
@@ -11,14 +10,8 @@ export default function Header() {
     <header className="border-b border-line">
       <div className="max-w-6xl mx-auto px-6 py-5 flex items-center justify-between">
         <Link href="/" className="flex items-center gap-3">
-          <Image
-            src="/logo.png"
-            alt="Artiza Studio"
-            width={40}
-            height={40}
-            className="h-10 w-auto"
-            priority
-          />
+          {/* eslint-disable-next-line @next/next/no-img-element */}
+          <img src="/logo.svg" alt="Artiza Studio" className="h-10 w-auto" />
           <span className="font-display italic text-2xl tracking-tight">
             Artiza Studio
           </span>
